66f5bdc3d95d84c3488935c8161e883372c14626
Andreas Kozadinos <koza-sparrow@hotmail.com>
2025-09-19 16:58:17 +0300

    Refactor OID-Fed Settings (Bug Fix) RCIAM[254]

--- js/apps/admin-ui/src/openid-federation/GeneralSettings.tsx
+++ js/apps/admin-ui/src/openid-federation/GeneralSettings.tsx
@@ -587,7 +587,7 @@ export const OpenIdFederationGeneralSettings = ({
                         />
                       }
                       validated={
-                        errors.openIdFederationOPClientRegistrationTypesSupported
+                        errors.openIdFederationRPClientRegistrationTypesSupported
                           ? ValidatedOptions.error
                           : ValidatedOptions.default
                       }
